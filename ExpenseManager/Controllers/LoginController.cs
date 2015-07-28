05c1c94bc1bacdb3b282d9fbb2081c478fda7451
Valentine Radchuk <zaknafeyn@gmail.com>
2015-07-28 14:36:36 +0300

    Fix hash function

--- ExpenseManager/Controllers/LoginController.cs
+++ ExpenseManager/Controllers/LoginController.cs
@@ -14,7 +14,7 @@ namespace ExpenseManager.Controllers
     [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class LoginController : BaseApiController
     {
-        private MD5 _hashFunc = MD5.Create();
+        private MD5 _hashFunc = MD5.Create(); 
 
         // GET api/login
         public LoginController(ExpenseManagerContext ctx) : base(ctx)
@@ -28,23 +28,24 @@ namespace ExpenseManager.Controllers
             if (Request.Method.Method.ToUpper() == "OPTIONS")
                 return null;
 
-            var result = _ctx.Profiles.SingleOrDefault(x => GetHash(x.Login) == loginData.LoginHash && x.PasswordHash == loginData.PasswordHash.Trim());
+            var result = _ctx.Profiles.ToList().SingleOrDefault(x => GetHash(x.Login) == loginData.LoginHash && x.PasswordHash == loginData.PasswordHash.Trim());
             
             return result;
         }
 
-        internal string GetHash(string @string)
+        private string GetHash(string @string, bool upperCase = false)
         {
             var inputBytes = Encoding.ASCII.GetBytes(@string);
             byte[] hash = _hashFunc.ComputeHash(inputBytes);
 
-            var sb = new StringBuilder();
-            for (int i = 0; i < hash.Length; i++)
-            {
-                sb.Append(i.ToString("X2"));
-            }
+            var result = new StringBuilder(hash.Length * 2);
 
-            return sb.ToString();
+            for (int i = 0; i < hash.Length; i++)
+                result.Append(hash[i].ToString(upperCase ? "X2" : "x2"));
+
+//            System.Diagnostics.Trace.WriteLine("Converting string " + @string + ". Resulting hash: " + result);
+
+            return result.ToString();
         }
     }
 }
