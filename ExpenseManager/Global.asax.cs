ab65f8ac9085e392be0ae18a75a90ebb86bccbb3
Valentine Radchuk <zaknafeyn@gmail.com>
2015-08-05 16:57:44 +0300

    Small refactoring

--- ExpenseManager/Global.asax.cs
+++ ExpenseManager/Global.asax.cs
@@ -13,7 +13,7 @@ namespace ExpenseManager
     // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
     // visit http://go.microsoft.com/?LinkId=9394801
 
-    public class WebApiApplication : System.Web.HttpApplication
+    public class WebApiApplication : HttpApplication
     {
         protected void Application_Start()
         {
